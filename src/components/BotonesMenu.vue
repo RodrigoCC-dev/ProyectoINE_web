20a58656c03afa502f118272315802b448b43b81
Rodrigo <rodrigo.castillo.c@usach.cl>
2020-02-03 02:28:39 -0300

    Se corrigen direcciones de botones con B-buttons

--- src/components/BotonesMenu.vue
+++ src/components/BotonesMenu.vue
@@ -1,10 +1,10 @@
 <template>
-  <div>
+  <div class="mt-5">
     <div>
       <p class="lead">Elija perfil de usuario a utilizar:</p>
     </div>
     <div v-for="item of perfiles" :key="item.id">
-      <button  type="button" class="btn btn-outline-primary col-8" :href="item.direccion">{{item.nombre}}</button>
+      <b-button  variant="outline-primary" class="col-8" :href="item.direccion">{{item.nombre}}</b-button>
       <p></p>
     </div>
   </div>
