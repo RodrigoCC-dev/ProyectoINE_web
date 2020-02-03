51afcb897494687b2b804163aae83764aebefffb
Rodrigo <rodrigo.castillo.c@usach.cl>
2020-02-03 03:29:20 -0300

    Importación de Axios en el proyecto

--- src/components/BotonesMenu.vue
+++ src/components/BotonesMenu.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="mt-5">
-    <div>
+    <div class="mt-5 mb-4">
       <p class="lead">Elija perfil de usuario a utilizar:</p>
     </div>
     <div v-for="item of perfiles" :key="item.id">
-      <b-button  variant="outline-primary" class="col-8" :href="item.direccion">{{item.nombre}}</b-button>
-      <p></p>
+      <b-button variant="outline-primary" class="col-6 mt-4" :href="item.direccion">{{item.nombre}}</b-button>
+      <span class="col-6"></span>
+      <span class="badge badge-pill badge-info">?</span>
     </div>
   </div>
 </template>
@@ -19,6 +20,11 @@
     name: 'BotonesMenu',
     props: {
       msg: String
+    },
+    data(){
+      return{
+        mainProps: {width: 75, heigth: 75, class: 'm1'}
+      }
     },
     computed: {
       ...mapState(['perfiles'])
--- src/main.js
+++ src/main.js
@@ -8,6 +8,10 @@ Vue.use(BootstrapVue)
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+Vue.use(VueAxios, axios)
+
 Vue.config.productionTip = false
 
 new Vue({
